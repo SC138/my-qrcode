5a702b8bcb102b6ead4730888c52f93487b382ea
Thomas Chevron <chevron.thomas33@gmail.com>
2024-07-10 17:19:58 +0200

    test animations

--- src/Links.jsx
+++ src/Links.jsx
@@ -4,9 +4,9 @@ const Links = () => {
   const links = [
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/thomas-chevron/' },
     { name: 'GitHub', url: 'https://github.com/SC138' },
+    { name: 'Mon CV', url: '/documents/cv.pdf' },
     { name: 'Email', url: 'mailto:chevron.thomas33@gmail.com' },
     { name: 'Téléphone', url: 'tel:+33698422922' },
-    { name: 'Mon CV', url: '/documents/cv.pdf' },
   ];
 
   return (
